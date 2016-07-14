5e2b63e3a2a1019617e91d439ccbd239e6310c8e
bturcott <bradford727@gmail.com>
2016-07-14 16:52:56 -0700

    Updated blackjack

--- milestone_project_2.py
+++ milestone_project_2.py
@@ -21,7 +21,35 @@ class Player(object):
 		self.bankroll += amount
 
 class Deck(object):
-	pass
+	card_list = ['one','one','one','one',
+			'two', 'two', 'two', 'two',
+			'three', 'three', 'three', 'three',
+			'four', 'four', 'four', 'four',
+			'five', 'five', 'five', 'five',
+			'six', 'six', 'six', 'six', 'six',
+			'seven', 'seven', 'seven', 'seven',
+			'eight', 'eight', 'eight', 'eight',
+			'nine', 'nine', 'nine', 'nine',
+			'ten', 'ten', 'ten', 'ten',
+			'jack', 'jack', 'jack', 'jack',
+			'queen', 'queen', 'queen', 'queen',
+			'king', 'king', 'king', 'king',
+			'ace', 'ace', 'ace', 'ace']
+			
+	card_map = {'one':1, 'two':2, 'three':3, 
+				'four':4, 'five':5, 'six':6,
+				'seven':7, 'eight':8, 'nine':9,
+				'ten':10, 'jack':10, 'queen':10,
+				'king':10, 'ace':[11,1]}
+	
+	def __init__(self):
+		self.cards = Deck.card_list
+
+	def draw_card(self):
+		
+
+	def shuffle_deck(self):
+		self.cards = Deck.card_list
 
 brad = Player(10)
 print brad.wins
