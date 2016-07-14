8ec05a096cf129ccb7527b10ec6a65956a0013f5
Bradford Turcott <Bradford@Bradfords-Air.attlocal.net>
2016-07-13 21:43:55 -0700

    minor updates to blackjack project

--- milestone_project_2.py
+++ milestone_project_2.py
@@ -11,14 +11,20 @@ Requirements:
 """
 
 class Player(object):
+	wins = 0
+	losses = 0
 	
-	def __init__(self,bankroll=100)
+	def __init__(self,bankroll=100):
 		self.bankroll = bankroll
 
-	def adjust_bankroll(self,amount)
+	def adjust_bankroll(self,amount):
 		self.bankroll += amount
 
 class Deck(object):
 	pass
 
+brad = Player(10)
+print brad.wins
+print brad.losses
+print brad.bankroll
 
